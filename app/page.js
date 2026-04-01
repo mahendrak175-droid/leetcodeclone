29a8e4a4d3cd949599b56f8980fe492112fe36a6
mahendra <mahendrak175@gmail.com>
2026-04-01 23:08:23 +0530

    prisma and postgres

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
+        Welcome to Next.js 13!
+        <Button>Click me</Button>
+      </h1>
+    </div>
+  );
+}
